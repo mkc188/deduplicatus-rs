6ca8692ebb19dc40348b8abac94f5a1b54a70af6
Mok Ka Chun <mkc188@users.noreply.github.com>
2015-10-11 23:12:41 +0800

    Install rsnotify

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate clap;
+extern crate notify;
 
 use clap::{App, SubCommand};
+use notify::{RecommendedWatcher, Error, Watcher};
+use std::sync::mpsc::channel;
 
 fn main() {
 
@@ -76,4 +79,26 @@ fn main() {
 
 
     // Continued program logic goes here...
+
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let mut w: Result<RecommendedWatcher, Error> = Watcher::new(tx);
+
+    match w {
+        Ok(mut watcher) => {
+            // Add a path to be watched. All files and directories at that path and
+            // below will be monitored for changes.
+            watcher.watch("/home/test/notify");
+
+            // You'll probably want to do that in a loop. The type to match for is
+            // notify::Event, look at src/lib.rs for details.
+            match rx.recv() {
+                _ => println!("Recv.")
+            }
+        },
+        Err(e) => println!("Error")
+    }
 }
